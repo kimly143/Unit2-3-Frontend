5f63463eb101dbb234ad9ee0806b6b86062b25ad
Kim Ly Buck <kim.3993@gmail.com>
2020-05-24 19:04:35 -0500

    setting up basic

--- schoolinthecloud/src/App.js
+++ schoolinthecloud/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+
+import store from './store/store';
+import Volunteer from './components/forms/Volunteer';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Provider store={store}>
+			<div className="App">
+				<Volunteer />
+			</div>
+		</Provider>
+	);
 }
 
 export default App;
---
+++ schoolinthecloud/src/components/forms/Volunteer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+export default function Volunteer(){
+
+    const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+    return <h1> Hi Volunteer!</h1>
+    <Button>Normal</Button>
+    <Button primary>Primary</Button>
+}
+
