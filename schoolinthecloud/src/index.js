5260258a3f99a0b65ab98462d468f3d34b534204
daniel payne <haroldfranklinpayne@gmail.com>
2020-05-24 22:06:56 -0400

    pulled from master with my changes

--- schoolinthecloud/src/index.js
+++ schoolinthecloud/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./assets/styles/css/index.css";
 import App from './App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { rootReducer } from './store/reducers/rootReducer';
+// import { rootReducer } from './store/reducers/rootReducer';
 
 const root = document.querySelector('#root');
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+// const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
+		{/*<Provider store={store}>*/}
 			<App />
-		</Provider>
+		{/*</Provider>*/}
 	</React.StrictMode>,
 	root
 );
