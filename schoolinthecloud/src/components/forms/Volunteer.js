e0ceedf107a84983685a29744172af474b58e279
Kim Ly Buck <kim.3993@gmail.com>
2020-05-25 20:55:10 -0500

    setting up basic

--- schoolinthecloud/src/components/forms/Volunteer.js
+++ schoolinthecloud/src/components/forms/Volunteer.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 
-
-export default function Volunteer(){
-
-    const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
+const Form= styled.form`
+    background: lightblue;
 `;
-    return <h1> Hi Volunteer!</h1>
-    <Button>Normal</Button>
-    <Button primary>Primary</Button>
-}
 
+const 
+const Button = styled.button`
+	background: tomato;
+	color: blue;
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border: 2px solid palevioletred;
+	border-radius: 3px;
+`;
+
+
+
+export default function Volunteer() {
+	return (
+		<div>
+			<h1> Hi Volunteer!</h1>
+            {/* 1. Name
+		2. Email
+		3. Parent phone/Volunteer phone
+		4. Location/address
+		5. Day/Time availability
+		6. Terms and conditions agreement
+        7. Text field for bio/prompt for statement */}
+            <Form>
+
+            </Form>
+			<Button> Submit </Button>
+			<Button>Cancel</Button>
+		</div>
+	);
+}
