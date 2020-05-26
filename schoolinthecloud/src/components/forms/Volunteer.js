295d98e34e39e563f2dd0c57d6759f5f4ecd46f1
Kim Ly Buck <kim.3993@gmail.com>
2020-05-26 01:17:40 -0500

    add useform hooks

--- schoolinthecloud/src/components/forms/Volunteer.js
+++ schoolinthecloud/src/components/forms/Volunteer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components'; 
+
+import useForm from '../../hooks/useForm'
 
 const Form = styled.div`
 	margin: 0 auto;
@@ -17,13 +19,13 @@ const InputWrapper = styled.div`
 	flex-direction: row;
 	flex-wrap: nowrap;
 	justify-content: flex-start;
-    margin-top: 10px;
-    justify-content: center;
-
+	margin-top: 10px;
 `;
 
-const Label = styled.label`width: 35%;
-padding: 10px;`;
+const Label = styled.label`
+	width: 30%;
+	padding: 10px;
+`;
 
 const Input = styled.input`width: 60%;`;
 
@@ -36,7 +38,27 @@ const Button = styled.button`
 	margin: 10px;
 `;
 
+const ButtonWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: center;
+	margin-top: 10px;
+`;
+
 export default function Volunteer() {
+
+    const [ formState, changeHandler ] = useForm({
+		name: '',
+        email: '',
+        phone:'',
+        location:'',
+        availability:'',
+        bio:'',
+        term:''
+    });
+    
 	return (
 		<div class="volunteer">
 			<h1> Hello Volunteer! Please enter your information</h1>
@@ -44,40 +66,76 @@ export default function Volunteer() {
 			<Form>
 				<InputWrapper>
 					<Label>Name: </Label>
-					<Input type="text" name="name" id="name" required />
+                    <Input type="text" 
+                    name="name" 
+                    id="name" required
+                    value={formState.name}
+				onChange={changeHandler}/>
 				</InputWrapper>
 				<InputWrapper>
 					<Label for="email">Email: </Label>
-					<Input type="email" name="email" id="email" required />
+                    <Input type="email" 
+                    name="email" 
+                    id="email" required 
+                    value={formState.email}
+				onChange={changeHandler}/>
 				</InputWrapper>
 				<InputWrapper>
 					<Label for="phone">Phone: </Label>
-					<Input type="phone" name="phone" id="phone" required />
+                    <Input type="phone" 
+                    name="phone" 
+                    id="phone" required 
+                    value={formState.phone}
+				onChange={changeHandler}/>
 				</InputWrapper>
 				<InputWrapper>
 					<Label for="Location">Location: </Label>
-					<Input type="text" name="Location" id="Location" required />
+                    <Input type="text" 
+                    name="location" 
+                    id="Location" 
+                    required 
+                    value={formState.location}
+				onChange={changeHandler}/>
 				</InputWrapper>
 				<InputWrapper>
 					<Label for="availability">Availability: </Label>
-					<Input type="text" name="email" id="email" required />
+                    <Input type="text" 
+                    name="availability" 
+                    id="availability" 
+                    required 
+                    value={formState.availability}
+				onChange={changeHandler}/>
 				</InputWrapper>
 				<InputWrapper>
 					<Label for="bio">Bio: </Label>
-					<Input type="text" name="bio" id="bio" required />
+                    <Input type="text" 
+                    name="bio" 
+                    id="bio" required 
+                    value={formState.bio}
+				onChange={changeHandler}/>
 				</InputWrapper>
 				<InputWrapper>
-					<Label>Terms and conditions </Label>
-					<Input type="text" name="name" id="name" required />
+					<Label>
+                        <input class="form-check-input" 
+                        type="checkbox" value="" 
+                        id="defaultCheck1"/> 
+					</Label>
+                    <Input type="text" 
+                    name="term" 
+                    id="term" 
+                    placeholder="Term and Condiditon Agreement..." 
+                    readonly 
+                    value={formState.term}
+				onChange={changeHandler}/>
 				</InputWrapper>
-				<InputWrapper>
-					<Button type="submit" value="Submit!">
+				<ButtonWrapper>
+					<Button type="submit" value="Submit">
 						Submit
 					</Button>
-					<Button type="cancel" value="cancel!">
+					<Button type="cancel" value="cancel">
 						Cancel
 					</Button>
-				</InputWrapper>
+				</ButtonWrapper>
 			</Form>
 		</div>
 	);
