2ab0acb9d418ee0b49dd2f5dedc66413b61ee220
Kim Ly Buck <kim.3993@gmail.com>
2020-05-27 19:04:39 -0500

    finished volunteer form, styled.

--- schoolinthecloud/src/components/forms/Volunteer.js
+++ schoolinthecloud/src/components/forms/Volunteer.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import styled from 'styled-components'; 
+import styled from 'styled-components';
+import axios from 'axios';
 
-import useForm from '../../hooks/useForm'
+import useForm from '../../hooks/useForm';
 
-const Form = styled.div`
+const Form = styled.form`
 	margin: 0 auto;
 	display: flex;
 	flex-wrap: wrap;
@@ -47,86 +48,123 @@ const ButtonWrapper = styled.div`
 	margin-top: 10px;
 `;
 
-export default function Volunteer() {
+const CheckboxLabel = styled.label`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: baseline;
+`;
 
-    const [ formState, changeHandler ] = useForm({
+export default function Volunteer() {
+	const [ formState, changeHandler ] = useForm({
 		name: '',
-        email: '',
-        phone:'',
-        location:'',
-        availability:'',
-        bio:'',
-        term:''
-    });
-    
+		email: '',
+		phone: '',
+		location: '',
+		availability: '',
+		bio: '',
+		term: false
+	});
+
+	//const [ token, setToken ] = useAuthToken();
+
+	//using async
+	const submitHandler = (event) => {
+		event.preventDefault();
+		console.log(formState);
+		// making a request to server for login, sending formState contain username and password to server
+		// await : make promise to resolve first then continue
+		const response = await axios.post('https://build-week-school-in-the-cloud.herokuapp.com/api', formState);
+		console.log(response.data);
+
+		//setToken(response.data.payload);
+		//props.history.push('/bubbles');
+	};
+
 	return (
-		<div class="volunteer">
+		<div className="volunteer">
 			<h1> Hello Volunteer! Please enter your information</h1>
 
-			<Form>
+			<Form onSubmit={submitHandler}>
 				<InputWrapper>
 					<Label>Name: </Label>
-                    <Input type="text" 
-                    name="name" 
-                    id="name" required
-                    value={formState.name}
-				onChange={changeHandler}/>
+					<Input type="text" name="name" id="name" required value={formState.name} onChange={changeHandler} />
 				</InputWrapper>
 				<InputWrapper>
-					<Label for="email">Email: </Label>
-                    <Input type="email" 
-                    name="email" 
-                    id="email" required 
-                    value={formState.email}
-				onChange={changeHandler}/>
+					<Label htmlFor="email">Email: </Label>
+					<Input
+						type="email"
+						name="email"
+						id="email"
+						required
+						value={formState.email}
+						onChange={changeHandler}
+					/>
 				</InputWrapper>
 				<InputWrapper>
-					<Label for="phone">Phone: </Label>
-                    <Input type="phone" 
-                    name="phone" 
-                    id="phone" required 
-                    value={formState.phone}
-				onChange={changeHandler}/>
+					<Label htmlFor="phone">Phone: </Label>
+					<Input
+						type="phone"
+						name="phone"
+						id="phone"
+						required
+						value={formState.phone}
+						onChange={changeHandler}
+					/>
 				</InputWrapper>
 				<InputWrapper>
-					<Label for="Location">Location: </Label>
-                    <Input type="text" 
-                    name="location" 
-                    id="Location" 
-                    required 
-                    value={formState.location}
-				onChange={changeHandler}/>
+					<Label htmlFor="Location">Location: </Label>
+					<Input
+						type="text"
+						name="location"
+						id="Location"
+						required
+						value={formState.location}
+						onChange={changeHandler}
+					/>
 				</InputWrapper>
 				<InputWrapper>
-					<Label for="availability">Availability: </Label>
-                    <Input type="text" 
-                    name="availability" 
-                    id="availability" 
-                    required 
-                    value={formState.availability}
-				onChange={changeHandler}/>
+					<Label htmlFor="availability">Availability: </Label>
+					<Input
+						type="text"
+						name="availability"
+						id="availability"
+						required
+						value={formState.availability}
+						onChange={changeHandler}
+					/>
 				</InputWrapper>
 				<InputWrapper>
-					<Label for="bio">Bio: </Label>
-                    <Input type="text" 
-                    name="bio" 
-                    id="bio" required 
-                    value={formState.bio}
-				onChange={changeHandler}/>
+					<Label htmlFor="bio">Bio: </Label>
+					<Input type="text" name="bio" id="bio" required value={formState.bio} onChange={changeHandler} />
 				</InputWrapper>
 				<InputWrapper>
 					<Label>
-                        <input class="form-check-input" 
-                        type="checkbox" value="" 
-                        id="defaultCheck1"/> 
+						<input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
 					</Label>
-                    <Input type="text" 
-                    name="term" 
-                    id="term" 
-                    placeholder="Term and Condiditon Agreement..." 
-                    readonly 
-                    value={formState.term}
-				onChange={changeHandler}/>
+					<Input
+						type="text"
+						name="term"
+						id="term"
+						placeholder="Term and Condiditon Agreement..."
+						readonly
+						value={formState.term}
+						onChange={changeHandler}
+					/>
+				</InputWrapper>
+				<InputWrapper>
+					<CheckboxLabel>
+						<input
+							type="checkbox"
+							name="term"
+							id="term"
+							checked={formState.term}
+							onChange={changeHandler}
+						/>
+						I agree to the terms and conditions.
+					</CheckboxLabel>
 				</InputWrapper>
 				<ButtonWrapper>
 					<Button type="submit" value="Submit">
