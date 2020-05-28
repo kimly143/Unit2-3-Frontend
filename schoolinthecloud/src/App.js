e40c1cd71b3dfe623d640fa79b48eab250f6ab44
Kim Ly Buck <kim.3993@gmail.com>
2020-05-27 19:56:08 -0500

    .

--- schoolinthecloud/src/App.js
+++ schoolinthecloud/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store/store';
-import Volunteer from './components/forms/Volunteer';
 import './assets/styles/css/index.css';
 import Header from './components/header/Header';
 
@@ -12,8 +11,7 @@ function App(props) {
 	return (
 		<Provider store={store}>
 			<div className="App">
-				{/* <Header props={props} /> */}
-				<Volunteer />
+				<Header props={props} />
 			</div>
 		</Provider>
 
