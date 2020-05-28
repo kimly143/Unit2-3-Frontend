3ebc06b076cf08a72262bf246b79d63757f12a87
Kim Ly Buck <kim.3993@gmail.com>
2020-05-27 20:03:13 -0500

    removed unuse store file

--- schoolinthecloud/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-const reducer = state => state; //dummy reducer.
-
-const store = createStore(reducer, applyMiddleware(thunk));
-
-export default store;
